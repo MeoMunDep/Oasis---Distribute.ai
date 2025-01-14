4987b5679cf1d64e46519fb1a7d776d64a199730
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-14 15:15:10 +0700

    updated use emails and passwords for longlive data

--- Oasis/setup.bat
+++ Oasis/setup.bat
@@ -53,13 +53,13 @@ goto MENU
 cls
 echo Creating configuration files...
 
-if not exist datas.txt (
-    type nul > datas.txt
-    echo Created datas.txt
+if not exist emails.txt (
+    type nul > emails.txt
+    echo Created emails.txt
 )
-if not exist wallets.txt (
-    type nul > wallets.txt
-    echo Created wallets.txt
+if not exist passwords.txt (
+    type nul > passwords.txt
+    echo Created passwords.txt
 )
 if not exist proxies.txt (
     type nul > proxies.txt
--- Oasis/setup.sh
+++ Oasis/setup.sh
@@ -83,7 +83,7 @@ while true; do
 
             check_configs
 
-            for file in datas.txt wallets.txt proxies.txt; do
+            for file in emails.txt passwords.txt proxies.txt; do
                 if [ ! -f "$file" ]; then
                     touch "$file"
                     print_green "Created $file"
